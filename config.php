41b8c34430e08b9750bec3426d964efdaeb376d9
IamTanaz <103096747+IamTanaz@users.noreply.github.com>
2022-04-06 19:22:33 +0600

    first commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+// $conn = mysqli_connect("localhost", "root", "", "crud_system") or die("connection failed!");
+$conn = mysqli_connect("localhost", "root", "", "employee_management_system") or die("connection failed!");
+?>
